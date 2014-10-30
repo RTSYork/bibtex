b67e1f4497397cff2d7442e4ec1b369cd62a80ff
Ian Gray <iangray001@github.com>
2014-10-30 15:08:46 +0000

    Removed polls app.

--- csbibtex/urls.py
+++ csbibtex/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-	url(r'^polls/', include('polls.urls', namespace="polls")),
 	url(r'^bibtex/', include('bibtex.urls', namespace="bibtex")),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
 )

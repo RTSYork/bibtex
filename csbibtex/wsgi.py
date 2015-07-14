e383af2a9a14893a3c888bf917ede4974df54f8a
I Gray <ig112@csresearch1.york.ac.uk>
2015-07-14 21:16:06 +0100

    Added wsgi settings and reset file to .gitignore for deployment

--- csbibtex/wsgi.py
+++ csbibtex/wsgi.py
@@ -8,7 +8,23 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csbibtex.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+"""
+From: http://projects.unbit.it/uwsgi/wiki/TipsAndTricks#uWSGIdjangoautoreloadmode
+"""
+"""
+import uwsgi
+from uwsgidecorators import timer
+from django.utils import autoreload
+
+@timer(3)
+def change_code_gracefull_reload(sig):
+    if autoreload.code_changed():
+        uwsgi.reload()
+"""

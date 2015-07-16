58db465447be15fffa82a4ce2ef220729d2fba17
I Gray <ig112@csresearch1.york.ac.uk>
2015-07-16 16:14:41 +0100

    Moved database into a separate subfolder to give nginx permission to access it.

--- csbibtex/settings.py
+++ csbibtex/settings.py
@@ -57,7 +57,7 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, "db", 'db.sqlite3'),
     }
 }
 

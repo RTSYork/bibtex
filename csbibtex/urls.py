6f89c8107b8a6ab2985bacea1664de7e5e463ca1
Ian Gray <iangray001@github.com>
2015-07-16 14:51:33 +0100

    Place admin as a sub-URL of bibtex

--- csbibtex/urls.py
+++ csbibtex/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+	url(r'^rts/bibtex/admin/', include(admin.site.urls)),
 	url(r'^rts/bibtex/', include('bibtex.urls', namespace="bibtex")),
-	url(r'^rts/admin/', include(admin.site.urls)),
 )

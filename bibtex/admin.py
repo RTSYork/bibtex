82cc42c302c853add6d31ea4d4ee35984eba3f82
Ian Gray <iangray001@github.com>
2015-07-26 16:42:51 +0100

    Added support for inline links for Docfiles, and Entry to be filtered by number of attached Docfiles

--- bibtex/admin.py
+++ bibtex/admin.py
@@ -1,11 +1,43 @@
+import os
 from django.contrib import admin
 from bibtex.models import Entry, Docfile
 from django.forms import TextInput, Textarea
 from django.db import models
+from django.conf import settings
 
-class DocInline(admin.StackedInline):
+class DocInline(admin.TabularInline):
+	def link(self, instance):
+		l = settings.MEDIA_URL + instance.filename
+		return "<a href=\"" + l + "\">" + l + "</a>"
+	link.allow_tags = True
 	extra = 0
 	model = Docfile
+	readonly_fields = ('link',)
+
+
+class FileNumFilter(admin.SimpleListFilter):
+	title = 'num files'
+	parameter_name = 'numfiles'
+
+	def lookups(self, request, model_admin):
+		return (
+			('0', '0'),
+			('1', '1'),
+			('more', '> 1'),
+		)
+
+	def queryset(self, request, queryset):
+		rv = set()
+		for item in queryset:
+			count = len(item.docfile_set.all())
+			if self.value() == '0' and count == 0:
+				rv.add(item.pk)
+			elif self.value() == '1' and count == 1:
+				rv.add(item.pk)
+			elif self.value() == 'more' and count > 1:
+				rv.add(item.pk)
+		return queryset.filter(pk__in = rv)
+
 
 
 class EntryAdmin(admin.ModelAdmin):
@@ -19,7 +51,7 @@ class EntryAdmin(admin.ModelAdmin):
 		'has_abstract'
 	)
 
-	list_filter = ['year', 'entered']
+	list_filter = ['entered', FileNumFilter, 'year']
 	search_fields = ['key', 'owner', 'author', 'title', 'abstract']
 	inlines = [DocInline]
 
